ae3f237e7fce3e0ffcac251662261554f589de4e
carlomagno-ale <carlomagno.ale@gmail.com>
2025-04-06 15:57:47 +0200

    Milestone 2: Refactor Home and MoviesContext components: replace clickFetch with handleMovie and handleTV for improved search functionality; update input handling and display logic for movies and series.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,22 +3,29 @@ import { useMovies } from "../contexts/MoviesContext";
 
 export default function Home() {
 
-    const { movies, clickFetch } = useMovies();
+    const { movies, handleMovie, seriesTV, handleTV } = useMovies();
 
     const [searchText, setSearchText] = useState('')
 
     return (
-
         <>
             <div>
-                <input
-                    type="text"
-                    placeholder="Cerca un film"
-                    onChange={(e) => setSearchText(e.target.value)}
-                />
-                <button onClick={() => clickFetch(searchText)}>
-                    Search
-                </button>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        handleMovie(searchText);
+                        handleTV(searchText);
+                        setSearchText('');
+                    }}
+                >
+                    <input
+                        type="text"
+                        placeholder="Cerca un film o una serie"
+                        value={searchText}
+                        onChange={(e) => setSearchText(e.target.value)}
+                    />
+                    <button type="submit">Cerca</button>
+                </form>
 
                 <section>
                     {movies.length > 0 ? (
@@ -28,12 +35,25 @@ export default function Home() {
                                     <li>{movie.title}</li>
                                     <li>Titolo originale: {movie.original_title}</li>
                                     <li>Lingua: {movie.original_language}</li>
-                                    <li>Voti: {movie.vote_count}</li>
+                                    <li>Voti: {movie.vote_average}</li>
                                 </ul>
                             ))}
                         </div>
                     ) : (
-
+                        <span></span>
+                    )}
+                    {seriesTV.length > 0 ? (
+                        <div>
+                            {seriesTV.map((series) => (
+                                <ul key={series.id}>
+                                    <li>{series.name}</li>
+                                    <li>Titolo originale: {series.original_name}</li>
+                                    <li>Lingua: {series.original_language}</li>
+                                    <li>Voti: {series.vote_average}</li>
+                                </ul>
+                            ))}
+                        </div>
+                    ) : (
                         <span></span>
                     )}
                 </section>
--- src/contexts/MoviesContext.jsx
+++ src/contexts/MoviesContext.jsx
@@ -7,10 +7,13 @@ const api_key = import.meta.env.VITE_MOVIE_DB_API_KEY;
 function MoviesProvider({ children }) {
 
     const [movies, setMovies] = useState([]);
+    const [seriesTV, setSeriesTV] = useState([]);
 
-    const urlMovie = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}`;
+    const urlMovie = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&include_adult=false&include_video=false`;
 
-    function clickFetch(searchText) {
+    const urlTV = `https://api.themoviedb.org/3/search/tv?api_key=${api_key}&include_adult=false&include_video=false`
+
+    function handleMovie(searchText) {
 
         fetch(urlMovie + `&query=${searchText}`)
             .then(res => res.json())
@@ -18,6 +21,23 @@ function MoviesProvider({ children }) {
 
                 setMovies(data.results);
 
+            })
+            .catch((error) => {
+                console.error("Error", error);
+            });
+    }
+
+    function handleTV(searchText) {
+
+        fetch(urlTV + `&query=${searchText}`)
+            .then(res => res.json())
+            .then(data => {
+
+                setSeriesTV(data.results);
+
+            })
+            .catch((error) => {
+                console.error("Error", error);
             });
     }
 
@@ -27,7 +47,10 @@ function MoviesProvider({ children }) {
             value={{
                 movies,
                 setMovies,
-                clickFetch
+                handleMovie,
+                seriesTV,
+                setSeriesTV,
+                handleTV
             }}
         >
             {children}
