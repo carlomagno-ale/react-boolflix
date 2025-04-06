6b67a7cdb71e20e9a67bae36c46dae6a422ec2ec
carlomagno-ale <carlomagno.ale@gmail.com>
2025-04-06 18:09:37 +0200

    Milestone 3: Add poster images for movies and series in Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,9 @@ export default function Home() {
 
     const { movies, handleMovie, seriesTV, handleTV } = useMovies();
 
-    const [searchText, setSearchText] = useState('')
+    const [searchText, setSearchText] = useState('');
+
+    const cover = "https://image.tmdb.org/t/p/w342";
 
     return (
         <>
@@ -34,6 +36,7 @@ export default function Home() {
                         <div>
                             {movies.map((movie) => (
                                 <ul key={movie.id}>
+                                    <img src={`${cover}${movie.poster_path}`} alt={`Poster di ${movie.title}`} />
                                     <li>Film: {movie.title}</li>
                                     <li>Titolo originale: {movie.original_title}</li>
                                     <li>Lingua: {flags[movie.original_language] ? (
@@ -52,6 +55,7 @@ export default function Home() {
                         <div>
                             {seriesTV.map((series) => (
                                 <ul key={series.id}>
+                                    <img src={`${cover}${series.poster_path}`} alt={`Poster di ${series.name}`} />
                                     <li>Serie: {series.name}</li>
                                     <li>Titolo originale: {series.original_name}</li>
                                     <li>Lingua: <img src={flags[series.original_language]} alt={series.original_language} /></li>
